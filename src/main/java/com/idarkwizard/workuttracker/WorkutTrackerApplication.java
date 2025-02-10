58e3f36eac7e3be2de227366f06395617a127b75
zalazarm <mzalazar@bdsol.com.ar>
2025-02-09 23:55:00 -0300

    Start Commit

---
+++ src/main/java/com/idarkwizard/workuttracker/WorkutTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.idarkwizard.workuttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkutTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkutTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idarkwizard/workuttracker/WorkutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idarkwizard.workuttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkutTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
